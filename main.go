c07774001b6764a9f28db4c38050c2312ee37fd8
syakod <hi@syakod.com>
2023-10-17 23:27:22 +0800

    default cors

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
 
 	router.ForwardedByClientIP = true
 	err := router.SetTrustedProxies([]string{"127.0.0.1"})
