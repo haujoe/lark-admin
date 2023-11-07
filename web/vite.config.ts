0f4b2498aa682ecc2a9116697d3ecc29152bfa56
syakod <hi@syakod.com>
2023-11-07 15:47:10 +0800

    vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,31 +1,96 @@
 import { URL, fileURLToPath } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import progress from 'vite-plugin-progress'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    progress(),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      dts: 'src/auto-imports.d.ts',
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      dts: 'src/components.d.ts',
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const viteEnv = loadEnv(mode, process.cwd()) as ImportMetaEnv
+  const { VITE_PUBLIC_PATH } = viteEnv
+
+  return {
+    // 公共基础路径。打包时根据实际情况修改
+    base: VITE_PUBLIC_PATH,
+    resolve: {
+      // 路径别名
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      /** 是否开启 HTTPS */
+      https: false,
+      /** 设置 host: true 才可以使用 Network 的形式，以 IP 访问项目 */
+      host: true, // host: "0.0.0.0"
+      /** 端口号 */
+      port: 5173,
+      /** 是否自动打开浏览器 */
+      open: false,
+      /** 跨域设置允许 */
+      cors: true,
+      /** 端口被占用时，是否直接退出 */
+      strictPort: false,
+      /** 接口代理 */
+      proxy: {
+        '/api/v1': {
+          target: 'https://baidu.com',
+          ws: true,
+          /** 是否允许跨域 */
+          changeOrigin: true,
+        },
+      },
+    },
+    build: {
+      /** 单个 chunk 文件的大小超过 2048KB 时发出警告 */
+      chunkSizeWarningLimit: 2048,
+      /** 禁用 gzip 压缩大小报告 */
+      reportCompressedSize: false,
+      /** 打包后静态资源目录 */
+      assetsDir: 'static',
+      rollupOptions: {
+        output: {
+          /**
+           * 分块策略
+           * 1. 注意这些包名必须存在，否则打包会报错
+           * 2. 如果你不想自定义 chunk 分割策略，可以直接移除这段配置
+           */
+          manualChunks: {
+            vue: ['vue', 'vue-router', 'pinia'],
+            element: ['element-plus', '@element-plus/icons-vue'],
+          },
+        },
+      },
+    },
+    /** 混淆器 */
+    esbuild: {
+      /** 打包时移除 console.log */
+      pure: ['console.log'],
+      /** 打包时移除 debugger */
+      drop: ['debugger'],
+      /** 打包时移除所有注释 */
+      legalComments: 'none',
+    },
+    /** Vite 插件 */
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
+        dts: 'src/auto-imports.d.ts',
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        dts: 'src/components.d.ts',
+        resolvers: [ElementPlusResolver()],
+      }),
+      progress(),
+    ],
+  }
 })
